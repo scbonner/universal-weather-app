66b3b6d2eef0b84d85c89e3d8b87f43967b90721
scbonner <codingbootcamp918@gmail.com>
2020-07-04 18:10:43 -0400

    "confirming changes"

---
+++ src/components/FiveDay.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import '/Normalize.css';
+
+
+const FiveDay = () => {
+
+    return (
+      <div className="main-content">
+        <h2>Featured: </h2>
+        <div class="jsx-1190245460 docCard">
+        <div class="jsx-4103317667 n-paper WeatherBar_root__2kwub">
+        <div class="WeatherBar_current__2RVb-">
+        <span>91<!-- -->°F</span>
+        <span><img src="https://static.particlenews.com/weather/sunny.png" alt="Sunny"></span></div>
+        <div class="WeatherBar_dailyBar__2VXPW">
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Sat</div>
+        <div><img src="https://static.particlenews.com/weather/sunny.png" alt="Sunny"></div>
+        <div><span>94<!-- -->°</span>
+        <span class="lowT">74<!-- -->°</span></div></div>
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Sun</div>
+        <div><img src="https://static.particlenews.com/weather/partlycloudy.png" alt="Partly Cloudy"></div>
+        <div>
+        <span>90<!-- -->°</span>
+        <span class="lowT">72<!-- -->°</span></div></div>
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Mon</div>
+        <div><img src="https://static.particlenews.com/weather/partlycloudy.png" alt="Partly Cloudy"></div>
+        <div>
+        <span>90<!-- -->°</span>
+        <span class="lowT">71<!-- -->°</span>
+        </div></div>
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Tue</div>
+        <div>
+        <img src="https://static.particlenews.com/weather/rain.png" alt="Rain"></div>
+        <div>
+        <span>84<!-- -->°</span>
+        <span class="lowT">73<!-- -->°</span></div></div>
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Wed</div>
+        <div>
+        <img src="https://static.particlenews.com/weather/rain.png" alt="Rain"></div>
+        <div>
+        <span>84<!-- -->°</span>
+        <span class="lowT">73<!-- -->°</span></div></div>
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Thu</div>
+        <div>
+        <img src="https://static.particlenews.com/weather/rain.png" alt="Rain"></div>
+        <div>
+        <span>86<!-- -->°</span>
+        <span class="lowT">74<!-- -->°</span></div></div>
+        <div class="WeatherBar_weekDay__2QIjK"><div class="day">Fri</div><div><img src="https://static.particlenews.com/weather/rain.png" alt="Rain"></div><div><span>90<!-- -->°</span>
+        <span class="lowT">74<!-- -->°</span></div></div>
+        <div class="WeatherBar_weekDay__2QIjK">
+        <div class="day">Sat</div>
+        <div>
+        <img src="https://static.particlenews.com/weather/sunny.png" alt="Sunny"></div>
+        <div><span>91<!-- -->°</span><span class="lowT">75<!-- -->°</span>
+        
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default FiveDay;
