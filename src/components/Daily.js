f5ddb162e65c79d262cce18c922f1e583b479d71
scbonner <codingbootcamp918@gmail.com>
2020-07-08 23:10:14 -0400

    "saving files in prep for first push to master"

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -1,3 +1,64 @@
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import WeatherCard from "./WeatherCard";
+// import moment from "moment";
+
+// export default ({ dateTime }) => {
+//   return (
+//     <div className={`d-flex flex-col border rounded pd cursor-pointer`}>
+//       <h4 className="m-0">{moment(dateTime * 1000).format("ddd, DD")}</h4>
+//     </div>
+//   );
+// };
+
+
+// const fetchWeatherData = () =>
+//   axios.get(
+//     "https://api.openweathermap.org/data/2.5/forecast/daily?id=1581129&APPID=f5021250979d6a49f4005170d1cf82a3&units=metric"
+//   );
+
+// export default function Home() {
+//   const [city, setCity] = useState({});
+
+//   const [daily, setDaily] = useState([]);
+
+//   useEffect(() => {
+//     fetchWeatherData().then(res => {
+//       console.log(res);
+//       setCity(res.data.city);
+//       setDaily(res.data.list);
+//     });
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>Weather Forecast</h1>
+//       <p>
+//         Location:
+//         <b>
+//           {city.name}, {city.country}
+//         </b>
+//       </p>
+//       <div className="d-flex">
+//         {daily.map((day, i) => (
+//           <WeatherCard
+//             key={i}
+//             className="mg"
+//             dateTime={day.dt}
+//             icon={day.weather[0].icon}
+//             main={day.weather[0].main}
+//             temp={day.temp.day}
+//             feelsTemp={day.feels_like.day}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+
+
+
+
 // import React, {Component} from 'react';
 // // import "./CurrentWeather.css"
 
--- src/components/FiveDay.js
+++ src/components/FiveDay.js
@@ -1,6 +1,5 @@
 // import React from 'react';
 
-// import Converter from './Converter';
 
 // import {
 //   Link,
@@ -14,20 +13,22 @@
 //     {
 //         [...Array(5).keys()].map(n => {
 //             return <li key={n}>
-//                     <Link to={`${match.url}/${n+1}`}>
+//                     <Link to={`${match.base}/${n+1}`}>
 //                       Converter {n+1}
 //                     </Link>
 //                   </li>;
 //         })
 //     }
 //     </ul>
+//     <BrowserRouter>
 //     <Switch>
-//       <Route path={`${match.url}/:id(\\d+)`} component={Converter} />
+//       <Route path={`${match.base}/:id(\\d+)`} component={FiveDay} />
 //       <Route
-//         path={match.url}
+//         path={match.base}
 //         render={() => <h3>Please select a message</h3>}
 //       />
 //     </Switch>
+//     </BrowserRouter>
 //   </div>
 // );
 
@@ -38,52 +39,52 @@
 
 
 
-// // import React, {Component} from 'react';
-// // import {BrowserRouter, Link, Route} from "react-router-dom";
-// // // import "./WeeklyForecast.css";
-// // import SearchWeather from "./SearchWeather";
+// import React, {Component} from 'react';
+// import {BrowserRouter, Link, Route} from "react-router-dom";
+// // import "./WeeklyForecast.css";
+// import SearchWeather from "./SearchWeather";
 
-// // class FiveDay extends Component {
+// class FiveDay extends Component {
 
-// //     render() {
-// //         const items = this.props.daily_weather;
-// //         const forecast_dates = this.props.forecast_dates;
-// //         let fc_dates_counter = 0;
-// //         const listItems = items.map(item =>
-// //         {
-// //             let days_div =  (
-// //                 <div key={item.dt} className="col-md-1">
+//     render() {
+//         const items = this.props.daily_weather;
+//         const forecast_dates = this.props.forecast_dates;
+//         let fc_dates_counter = 0;
+//         const listItems = items.map(item =>
+//         {
+//             let days_div =  (
+//                 <div key={item.dt} className="col-md-1">
 
-// //                         <div className="row">
-// //                             <Link to={`/${forecast_dates[fc_dates_counter]}`}>
-// //                                 <div className="fc-day col-md-12"> {this.props.convert_unix_to_date(item.dt)} </div>
-// //                             </Link>
-// //                             <div className="col-md-12"> 
-// //                             <img  src={ this.props.icon_address + item.weather[0].icon + ".png"} alt={item.weather[0].description} /> </div>
-// //                             <div className="fc-temp-font col-md-12"> <span className="fc-display" style={{paddingRight: '5px'}}> {parseFloat(item.temp.max).toFixed(0)} </span> <span className="fc-display" > {parseFloat(item.temp.min).toFixed(0)} </span> </div>
-// //                         </div>
+//                         <div className="row">
+//                             <Link to={`/${forecast_dates[fc_dates_counter]}`}>
+//                                 <div className="fc-day col-md-12"> {this.props.convert_unix_to_date(item.dt)} </div>
+//                             </Link>
+//                             <div className="col-md-12"> 
+//                             <img  src={ this.props.icon_address + item.weather[0].icon + ".png"} alt={item.weather[0].description} /> </div>
+//                             <div className="fc-temp-font col-md-12"> <span className="fc-display" style={{paddingRight: '5px'}}> {parseFloat(item.temp.max).toFixed(0)} </span> <span className="fc-display" > {parseFloat(item.temp.min).toFixed(0)} </span> </div>
+//                         </div>
 
-// //                 </div>
+//                 </div>
 
-// //             );
+//             );
 
-// //             fc_dates_counter++;
-// //             return days_div;
-// //         });
+//             fc_dates_counter++;
+//             return days_div;
+//         });
 
-// //         return (
-// //             <div className="row">
-// //                 <BrowserRouter>
-// //                     {listItems}
+//         return (
+//             <div className="row">
+//                 <BrowserRouter>
+//                     {listItems}
 
-// //                     <Route path={`/:date`} render={
-// //                         ({match}) => ( <SearchWeather
-// //                                 current_data={this.props.sorted_forecast_data[match.params.date]} /> )
-// //                     } />
-// //                 </BrowserRouter>
-// //             </div>
-// //         );
-// //     }
-// // }
+//                     <Route path={`/:date`} render={
+//                         ({match}) => ( <SearchWeather
+//                                 current_data={this.props.sorted_forecast_data[match.params.date]} /> )
+//                     } />
+//                 </BrowserRouter>
+//             </div>
+//         );
+//     }
+// }
 
 // // export default FiveDay;
